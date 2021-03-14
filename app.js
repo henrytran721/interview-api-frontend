cdfdc4dc12a24f78b73b016464e6815d7699f0a1
henrytran721 <henryhtran07@gmail.com>
2021-03-14 16:27:08 -0700

    configured server to serve frontend files

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const path = require('path');
+const dotenv = require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
 
-app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
